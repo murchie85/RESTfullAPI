9aa1750d48a607538fa344400673fd4d2b27b3bf
Adam <murchie85@gmail.com>
2017-04-27 22:03:14 +0100

    JSON

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -14,9 +14,23 @@ $app->get('/api/customers', function(Request $request, Response $response){
 			$db = new db();
 			// connect 
 			$db = $db->connect(); 
+			// when we are working with PDO we need to  create statements 
+			// there are a few benefits to creating prepared statements
+			// 1. the query only needs to be parsed once 
+			// 2. can be executed multiple times with the same / or diff peramaters
+			// 3. the parameters don't need to be quoted. 
+			//stmt is standard 
+			// passes in $sql var
 
-		}catch(PDOException $e){
+
+			$stmt = $db->query($sql);
+			$customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+			$db = null;
+			echo json_encode($customers);
+
+
+		} catch(PDOException $e){
 			//e for errors exception handling
-        echo '{"error": {"text": '.$e->getMessage().'}';
+       		 echo '{"error": {"text": '.$e->getMessage().'}';
     	}
 });
